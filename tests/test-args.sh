6d8f580fba8df7482dc9f25dec69371750413137
Niklas Bettgen <niklas@bettgen.de>
2024-01-30 16:23:38 +0100

    Check help option

--- tests/test-args.sh
+++ tests/test-args.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Check invalid argument format (exit codes only)
+# Check argument format (exit codes only)
+assert_eq "$($NONSEMVER -h            &> /dev/null)$?" "0" "Argument check"
+assert_eq "$($NONSEMVER --help        &> /dev/null)$?" "0" "Argument check"
+
 assert_eq "$($NONSEMVER               &> /dev/null)$?" "1" "Argument check"
+
 assert_eq "$($NONSEMVER foo           &> /dev/null)$?" "2" "Argument check"
 assert_eq "$($NONSEMVER foo123        &> /dev/null)$?" "2" "Argument check"
 assert_eq "$($NONSEMVER foo.bar.123   &> /dev/null)$?" "2" "Argument check"
