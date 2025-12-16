330e3a0d830a4a79bc0e6f0b87f7d069e2f171ed
Niklas Bettgen <niklas@bettgen.de>
2025-12-16 22:24:53 +0100

    More precise message for test asserts

--- tests/args.test.sh
+++ tests/args.test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Check argument format (exit codes only)
-assert_eq "$($NONSEMVER -h            &> /dev/null)$?" "0" "Argument check"
-assert_eq "$($NONSEMVER --help        &> /dev/null)$?" "0" "Argument check"
+assert_eq "$($NONSEMVER -h            &> /dev/null)$?" "0" "Exit code 0 for help"
+assert_eq "$($NONSEMVER --help        &> /dev/null)$?" "0" "Exit code 0 for help"
 
-assert_eq "$($NONSEMVER               &> /dev/null)$?" "1" "Argument check"
+assert_eq "$($NONSEMVER               &> /dev/null)$?" "1" "Exit code 1 without arguments"
 
-assert_eq "$($NONSEMVER foo           &> /dev/null)$?" "2" "Argument check"
-assert_eq "$($NONSEMVER foo123        &> /dev/null)$?" "2" "Argument check"
-assert_eq "$($NONSEMVER foo.bar.123   &> /dev/null)$?" "2" "Argument check"
-assert_eq "$($NONSEMVER 1.2           &> /dev/null)$?" "2" "Argument check"
-assert_eq "$($NONSEMVER 12...33       &> /dev/null)$?" "2" "Argument check"
-assert_eq "$($NONSEMVER 12.34.56.78   &> /dev/null)$?" "2" "Argument check"
-assert_eq "$($NONSEMVER 12.345.678    &> /dev/null)$?" "2" "Argument check"
-assert_eq "$($NONSEMVER 1234.56.78    &> /dev/null)$?" "2" "Argument check"
-assert_eq "$($NONSEMVER 123456789     &> /dev/null)$?" "2" "Argument check"
+assert_eq "$($NONSEMVER foo           &> /dev/null)$?" "2" "Exit code 2 for invalid argument"
+assert_eq "$($NONSEMVER foo123        &> /dev/null)$?" "2" "Exit code 2 for invalid argument"
+assert_eq "$($NONSEMVER foo.bar.123   &> /dev/null)$?" "2" "Exit code 2 for invalid argument"
+assert_eq "$($NONSEMVER 1.2           &> /dev/null)$?" "2" "Exit code 2 for invalid argument"
+assert_eq "$($NONSEMVER 12...33       &> /dev/null)$?" "2" "Exit code 2 for invalid argument"
+assert_eq "$($NONSEMVER 12.34.56.78   &> /dev/null)$?" "2" "Exit code 2 for invalid argument"
+assert_eq "$($NONSEMVER 12.345.678    &> /dev/null)$?" "2" "Exit code 2 for invalid argument"
+assert_eq "$($NONSEMVER 1234.56.78    &> /dev/null)$?" "2" "Exit code 2 for invalid argument"
+assert_eq "$($NONSEMVER 123456789     &> /dev/null)$?" "2" "Exit code 2 for invalid argument"
--- tests/bump.test.sh
+++ tests/bump.test.sh
@@ -7,24 +7,23 @@ ny=$((cy + 1))
 py=$((cy - 1))
 
 
-# Check exceeding of version limits
-assert_eq "$($NONSEMVER --bump-minor 12.34.9956 &> /dev/null)$?" "3" "Version limit"
-assert_eq "$($NONSEMVER --bump-bugfix 12.34.5699 &> /dev/null)$?" "3" "Version limit"
-
-
 # Check minor/bugfix version bump
-assert_eq "$($NONSEMVER --bump-minor "00.00.0000"  )" "00.$cy.0100" "Version bump"
-assert_eq "$($NONSEMVER --bump-minor "42.$py.1234" )" "42.$cy.0100" "Version bump"
-assert_eq "$($NONSEMVER --bump-minor "01.$ny.2468" )" "01.$ny.2500" "Version bump"
-assert_eq "$($NONSEMVER --bump-bugfix "69.00.0000"  )" "69.$cy.0100" "Version bump"
-assert_eq "$($NONSEMVER --bump-bugfix "99.$py.9999" )" "99.$cy.0100" "Version bump"
-assert_eq "$($NONSEMVER --bump-bugfix "01.$ny.2468" )" "01.$ny.2469" "Version bump"
+assert_eq "$($NONSEMVER --bump-minor "00.00.0000"  )" "00.$cy.0100" "Minor bump with invalid year"
+assert_eq "$($NONSEMVER --bump-minor "42.$py.1234" )" "42.$cy.0100" "Minor bump with earlier year"
+assert_eq "$($NONSEMVER --bump-minor "01.$ny.2468" )" "01.$ny.2500" "Minor bump with future year"
+assert_eq "$($NONSEMVER --bump-bugfix "69.00.0000"  )" "69.$cy.0100" "Bugfix bump with invalid year"
+assert_eq "$($NONSEMVER --bump-bugfix "99.$py.9999" )" "99.$cy.0100" "Bugfix bump with earlier year"
+assert_eq "$($NONSEMVER --bump-bugfix "01.$ny.2468" )" "01.$ny.2469" "Bugfix bump with future year"
 
-assert_eq "$($NONSEMVER --bump-minor "00.$cy.0000" )" "00.$cy.0100" "Version bump"
-assert_eq "$($NONSEMVER --bump-minor "12.$cy.3456" )" "12.$cy.3500" "Version bump"
-assert_eq "$($NONSEMVER --bump-minor "78.$cy.9012" )" "78.$cy.9100" "Version bump"
-assert_eq "$($NONSEMVER --bump-minor "78.$cy.9012" )" "78.$cy.9100" "Version bump"
+assert_eq "$($NONSEMVER --bump-minor "00.$cy.0000" )" "00.$cy.0100" "Regular minor bump"
+assert_eq "$($NONSEMVER --bump-minor "12.$cy.3456" )" "12.$cy.3500" "Regular minor bump"
+assert_eq "$($NONSEMVER --bump-minor "78.$cy.9012" )" "78.$cy.9100" "Regular minor bump"
+assert_eq "$($NONSEMVER --bump-minor "78.$cy.9012" )" "78.$cy.9100" "Regular minor bump"
 
-assert_eq "$($NONSEMVER --bump-bugfix "00.$cy.0000" )" "00.$cy.0001" "Version bump"
-assert_eq "$($NONSEMVER --bump-bugfix "12.$cy.3456" )" "12.$cy.3457" "Version bump"
-assert_eq "$($NONSEMVER --bump-bugfix "78.$cy.9012" )" "78.$cy.9013" "Version bump"
+assert_eq "$($NONSEMVER --bump-bugfix "00.$cy.0000" )" "00.$cy.0001" "Regular bugfix bump"
+assert_eq "$($NONSEMVER --bump-bugfix "12.$cy.3456" )" "12.$cy.3457" "Regular bugfix bump"
+assert_eq "$($NONSEMVER --bump-bugfix "78.$cy.9012" )" "78.$cy.9013" "Regular bugfix bump"
+
+# Check exceeding of numerical limits
+assert_eq "$($NONSEMVER --bump-bugfix "00.$cy.5599" )" "00.$cy.5600" "Overflow after bugfix bump"
+assert_eq "$($NONSEMVER --bump-minor 12.34.9956 &> /dev/null)$?" "3" "No more minor versions available"
