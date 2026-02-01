nbe95/kc-version

Export necessities only

tests/run.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-DIR="$(dirname "$0")"
-VERSION="$DIR/../version.sh"
+dir="$(dirname "$0")"
+VERSION="$dir/../version.sh"
 export VERSION
 
-source "$DIR/assert/assert.sh"
+source "$dir/assert/assert.sh"
 
-# Prepare current, next and previous year as variables
-CY="$(date +"%y")"
-NY=$((CY + 1))
-PY=$((CY - 1))
-export CY
-export NY
-export PY
 
 # Run separate unit tests
-source "$DIR/test-git.sh"
-source "$DIR/test-args.sh"
-source "$DIR/test-parse.sh"
-source "$DIR/test-bump.sh"
+source "$dir/test-git.sh"
+source "$dir/test-args.sh"
+source "$dir/test-parse.sh"
+source "$dir/test-bump.sh"

tests/test-bump.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
+
+# Prepare current, next and previous year as variables
+cy="$(date +"%y")"
+ny=$((cy + 1))
+py=$((cy - 1))
+
+
 # Check exceeding of version limits
 assert_eq "$($VERSION --major 12.34.9956    &> /dev/null)$?" "3" "Version limit"
 assert_eq "$($VERSION --minor 12.34.5699    &> /dev/null)$?" "3" "Version limit"
 
 
 # Check major/minor version bump
-assert_eq "$($VERSION --major "00.00.0000"  )" "00.$CY.0100" "Version bump"
-assert_eq "$($VERSION --major "42.$PY.1234" )" "42.$CY.0100" "Version bump"
-assert_eq "$($VERSION --major "01.$NY.2468" )" "01.$NY.2500" "Version bump"
-assert_eq "$($VERSION --minor "69.00.0000"  )" "69.$CY.0100" "Version bump"
-assert_eq "$($VERSION --minor "99.$PY.9999" )" "99.$CY.0100" "Version bump"
-assert_eq "$($VERSION --minor "01.$NY.2468" )" "01.$NY.2469" "Version bump"
+assert_eq "$($VERSION --major "00.00.0000"  )" "00.$cy.0100" "Version bump"
+assert_eq "$($VERSION --major "42.$py.1234" )" "42.$cy.0100" "Version bump"
+assert_eq "$($VERSION --major "01.$ny.2468" )" "01.$ny.2500" "Version bump"
+assert_eq "$($VERSION --minor "69.00.0000"  )" "69.$cy.0100" "Version bump"
+assert_eq "$($VERSION --minor "99.$py.9999" )" "99.$cy.0100" "Version bump"
+assert_eq "$($VERSION --minor "01.$ny.2468" )" "01.$ny.2469" "Version bump"
 
-assert_eq "$($VERSION --major "00.$CY.0000" )" "00.$CY.0100" "Version bump"
-assert_eq "$($VERSION --major "12.$CY.3456" )" "12.$CY.3500" "Version bump"
-assert_eq "$($VERSION --major "78.$CY.9012" )" "78.$CY.9100" "Version bump"
-assert_eq "$($VERSION --major "78.$CY.9012" )" "78.$CY.9100" "Version bump"
+assert_eq "$($VERSION --major "00.$cy.0000" )" "00.$cy.0100" "Version bump"
+assert_eq "$($VERSION --major "12.$cy.3456" )" "12.$cy.3500" "Version bump"
+assert_eq "$($VERSION --major "78.$cy.9012" )" "78.$cy.9100" "Version bump"
+assert_eq "$($VERSION --major "78.$cy.9012" )" "78.$cy.9100" "Version bump"
 
-assert_eq "$($VERSION --minor "00.$CY.0000" )" "00.$CY.0001" "Version bump"
-assert_eq "$($VERSION --minor "12.$CY.3456" )" "12.$CY.3457" "Version bump"
-assert_eq "$($VERSION --minor "78.$CY.9012" )" "78.$CY.9013" "Version bump"
+assert_eq "$($VERSION --minor "00.$cy.0000" )" "00.$cy.0001" "Version bump"
+assert_eq "$($VERSION --minor "12.$cy.3456" )" "12.$cy.3457" "Version bump"
+assert_eq "$($VERSION --minor "78.$cy.9012" )" "78.$cy.9013" "Version bump"

nbe95/NonSemVer

tests/run.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-DIR="$(dirname "$0")"
-NONSEMVER="$DIR/../NonSemVer.sh"
+dir="$(dirname "$0")"
+NONSEMVER="$dir/../NonSemVer.sh"
 export NONSEMVER
 
-source "$DIR/assert/assert.sh"
+source "$dir/assert/assert.sh"
 
-# Prepare current, next and previous year as variables
-CY="$(date +"%y")"
-NY=$((CY + 1))
-PY=$((CY - 1))
-export CY
-export NY
-export PY
 
 # Run separate unit tests
-source "$DIR/test-git.sh"
-source "$DIR/test-args.sh"
-source "$DIR/test-parse.sh"
-source "$DIR/test-bump.sh"
+source "$dir/test-git.sh"
+source "$dir/test-args.sh"
+source "$dir/test-parse.sh"
+source "$dir/test-bump.sh"

tests/test-bump.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
+
+# Prepare current, next and previous year as variables
+cy="$(date +"%y")"
+ny=$((cy + 1))
+py=$((cy - 1))
+
+
 # Check exceeding of version limits
 assert_eq "$($NONSEMVER --major 12.34.9956    &> /dev/null)$?" "3" "Version limit"
 assert_eq "$($NONSEMVER --minor 12.34.5699    &> /dev/null)$?" "3" "Version limit"
 
 
 # Check major/minor version bump
-assert_eq "$($NONSEMVER --major "00.00.0000"  )" "00.$CY.0100" "Version bump"
-assert_eq "$($NONSEMVER --major "42.$PY.1234" )" "42.$CY.0100" "Version bump"
-assert_eq "$($NONSEMVER --major "01.$NY.2468" )" "01.$NY.2500" "Version bump"
-assert_eq "$($NONSEMVER --minor "69.00.0000"  )" "69.$CY.0100" "Version bump"
-assert_eq "$($NONSEMVER --minor "99.$PY.9999" )" "99.$CY.0100" "Version bump"
-assert_eq "$($NONSEMVER --minor "01.$NY.2468" )" "01.$NY.2469" "Version bump"
+assert_eq "$($NONSEMVER --major "00.00.0000"  )" "00.$cy.0100" "Version bump"
+assert_eq "$($NONSEMVER --major "42.$py.1234" )" "42.$cy.0100" "Version bump"
+assert_eq "$($NONSEMVER --major "01.$ny.2468" )" "01.$ny.2500" "Version bump"
+assert_eq "$($NONSEMVER --minor "69.00.0000"  )" "69.$cy.0100" "Version bump"
+assert_eq "$($NONSEMVER --minor "99.$py.9999" )" "99.$cy.0100" "Version bump"
+assert_eq "$($NONSEMVER --minor "01.$ny.2468" )" "01.$ny.2469" "Version bump"
 
-assert_eq "$($NONSEMVER --major "00.$CY.0000" )" "00.$CY.0100" "Version bump"
-assert_eq "$($NONSEMVER --major "12.$CY.3456" )" "12.$CY.3500" "Version bump"
-assert_eq "$($NONSEMVER --major "78.$CY.9012" )" "78.$CY.9100" "Version bump"
-assert_eq "$($NONSEMVER --major "78.$CY.9012" )" "78.$CY.9100" "Version bump"
+assert_eq "$($NONSEMVER --major "00.$cy.0000" )" "00.$cy.0100" "Version bump"
+assert_eq "$($NONSEMVER --major "12.$cy.3456" )" "12.$cy.3500" "Version bump"
+assert_eq "$($NONSEMVER --major "78.$cy.9012" )" "78.$cy.9100" "Version bump"
+assert_eq "$($NONSEMVER --major "78.$cy.9012" )" "78.$cy.9100" "Version bump"
 
-assert_eq "$($NONSEMVER --minor "00.$CY.0000" )" "00.$CY.0001" "Version bump"
-assert_eq "$($NONSEMVER --minor "12.$CY.3456" )" "12.$CY.3457" "Version bump"
-assert_eq "$($NONSEMVER --minor "78.$CY.9012" )" "78.$CY.9013" "Version bump"
+assert_eq "$($NONSEMVER --minor "00.$cy.0000" )" "00.$cy.0001" "Version bump"
+assert_eq "$($NONSEMVER --minor "12.$cy.3456" )" "12.$cy.3457" "Version bump"
+assert_eq "$($NONSEMVER --minor "78.$cy.9012" )" "78.$cy.9013" "Version bump"